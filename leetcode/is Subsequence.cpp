deeac4a9b37352751d23cff42ea00fc84948bb13
Akshit-Paroha <akshitparoha22@gmail.com>
2024-12-07 00:22:10 +0530

    =

---
+++ leetcode/is Subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) 
+    {
+        if(s.empty())
+        {
+            return true;
+        }
+        if(s.length()>t.length())
+        {
+            return false;
+        }
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(s[j]==t[i])
+            {
+                j++;
+            }
+            if(j==s.length())
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
